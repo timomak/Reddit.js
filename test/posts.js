773a54a8a0d2a9bd579db8954de6c43a512b9717
timo <tmakhlay2@gmail.com>
2019-02-13 19:18:24 -0800

    Working posts

--- test/posts.js
+++ test/posts.js
@@ -1,4 +1,3 @@
-// test/posts.js
 const app = require("./../server");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
@@ -21,44 +20,36 @@ describe('Posts', function() {
       summary: 'post summary'
   };
   it('Should create with valid attributes at POST /posts/new', function(done) {
-  // Checks how many posts there are now
-  Post.estimatedDocumentCount()
-    .then(function (initialDocCount) {
-        chai
-            .request(app)
-            .post("/posts/new")
-            // This line fakes a form post,
-            // since we're not actually filling out a form
-            .set("content-type", "application/x-www-form-urlencoded")
-            // Make a request to create another
-            .send(newPost)
-            .then(function (res) {
-                Post.estimatedDocumentCount()
-                    .then(function (newDocCount) {
-                        // Check that the database has one more post in it
-                        expect(res).to.have.status(200);
-                        // Check that the database has one more post in it
-                        expect(newDocCount).to.be.equal(initialDocCount + 1)
-                        done();
-                    })
-                    .catch(function (err) {
-                        done(err);
-                    });
-            })
-            .catch(function (err) {
-                done(err);
-            });
-    })
-    .catch(function (err) {
-        done(err);
-    });
-});
-it('Should create with valid attributes at POST /posts/new', function(done) {
-
-// Mocha doesn't work
-
-});
-
-after(function () {
-  Post.findOneAndDelete(newPost);
+    // Checks how many posts there are now
+    Post.estimatedDocumentCount()
+      .then(function (initialDocCount) {
+          chai
+              .request(app)
+              .post("/posts/new")
+              // This line fakes a form post,
+              // since we're not actually filling out a form
+              .set("content-type", "application/x-www-form-urlencoded")
+              // Make a request to create another
+              .send(newPost)
+              .then(function (res) {
+                  Post.estimatedDocumentCount()
+                      .then(function (newDocCount) {
+                          // Check that the database has one more post in it
+                          expect(res).to.have.status(200);
+                          // Check that the database has one more post in it
+                          expect(newDocCount).to.be.equal(initialDocCount + 1)
+                          done();
+                      })
+                      .catch(function (err) {
+                          done(err);
+                      });
+              })
+              .catch(function (err) {
+                  done(err);
+              });
+      })
+      .catch(function (err) {
+          done(err);
+      });
+  });
 });
